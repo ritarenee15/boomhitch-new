4f6562e6b5f785a2f98ed65a4d0d7cc2faeaef4c
Madeline Jimenez <madelineijimenez@gmail.com>
2020-03-02 13:56:41 -0500

    File setup and basic layout

---
+++ js/script.js
@@ -0,0 +1,8 @@
+function menuCollapse() {
+    var x = document.getElementById("bottom-nav-bar");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
